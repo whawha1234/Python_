0a2334263fb11a1ce30caa51a40d227723794420
whawha1234 <ayane.pk.wha@gmail.com>
2026-09-12 16:17:40 +0700

    Add grade evaluation script in grade.py

---
+++ grade.py
@@ -0,0 +1,17 @@
+name = str(input("Enter your name"))
+grade = str(input("Enter your grade"))
+
+if grade == "A":
+    print("Excellent")
+elif grade == "B":
+    print("Great")
+elif grade == "C":
+    print("Good")
+elif grade == "D":
+    print("ok")
+elif grade == "E":
+    print("bad")
+elif grade == "F":
+    print("Fail")
+else:
+    print("Try again")
