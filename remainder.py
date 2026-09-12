73eb8a613f7907e1e98c5a551a666c005868d6ae
whawha1234 <ayane.pk.wha@gmail.com>
2026-09-12 16:42:09 +0700

    Create remainder.py

---
+++ remainder.py
@@ -0,0 +1,10 @@
+num1 = int(input("giv me a number"))
+num2 = int(input("give me another number"))
+
+remainder = num1 % num2
+
+if remainder == 0:
+    print("its an even number")
+else:
+    print("its an odd number")
+    
